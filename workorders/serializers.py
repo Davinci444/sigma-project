053d615947c32ed1861090069a32636c2f31ccaa
César Herrera <cesarherrera2608@gmail.com>
2025-08-27 14:58:21 -0500

    Lanzamos el multitask de las ordenes

--- workorders/serializers.py
+++ workorders/serializers.py
@@ -1,8 +1,22 @@
 # workorders/serializers.py
 from rest_framework import serializers
-from .models import WorkOrder, MaintenancePlan
+from .models import WorkOrder, MaintenancePlan, WorkOrderTask, WorkOrderPart
+
+class WorkOrderTaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkOrderTask
+        fields = '__all__'
+
+class WorkOrderPartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkOrderPart
+        fields = '__all__'
 
 class WorkOrderSerializer(serializers.ModelSerializer):
+    # Hacemos que las tareas y repuestos se puedan ver al consultar una OT
+    tasks = WorkOrderTaskSerializer(many=True, read_only=True)
+    parts_used = WorkOrderPartSerializer(many=True, read_only=True)
+
     class Meta:
         model = WorkOrder
         fields = '__all__'
--- workorders/urls.py
+++ workorders/urls.py
@@ -1,11 +1,18 @@
 # workorders/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WorkOrderViewSet, MaintenancePlanViewSet
+from .views import (
+    WorkOrderViewSet, 
+    MaintenancePlanViewSet, 
+    WorkOrderTaskViewSet, 
+    WorkOrderPartViewSet
+)
 
 router = DefaultRouter()
 router.register(r'workorders', WorkOrderViewSet)
 router.register(r'plans', MaintenancePlanViewSet)
+router.register(r'tasks', WorkOrderTaskViewSet) # Para todas las tareas
+router.register(r'parts', WorkOrderPartViewSet) # Para todos los repuestos
 
 urlpatterns = [
     path('', include(router.urls)),
